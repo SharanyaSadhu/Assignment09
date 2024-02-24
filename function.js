e50c5b1cbfb1fb9a21c05462b799c9ef4533f29c
SharanyaSadhu <157237520+SharanyaSadhu@users.noreply.github.com>
2024-02-24 11:27:02 -0500

    Update function.js

--- function.js
+++ function.js
@@ -15,5 +15,5 @@ app.get('/greet', (req, res) =>{
 });
 
 app.listen(port, () =>{
-    console.log('Server is running at http://localhost:${port');
+    console.log('Server is running at http://localhost:${port}');
 });
