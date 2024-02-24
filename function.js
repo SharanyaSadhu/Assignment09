ee845e217216192f8a11144181e467f080814ef7
SharanyaSadhu <157237520+SharanyaSadhu@users.noreply.github.com>
2024-02-24 11:26:10 -0500

    Create function.js

---
+++ function.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+function greetWithName(keyword){
+    const greeting = `Sharanya Sadhu says ${keyword}`;
+    return greeting;
+}
+
+app.get('/greet', (req, res) =>{
+    const keyword = req.query.keyword;
+    const result = greetWithName(keyword);
+    res.send(result);
+});
+
+app.listen(port, () =>{
+    console.log('Server is running at http://localhost:${port');
+});
